105a7ae6299bd01bacc6743077ce984225117ba1
Maria <fppl0101@gmail.com>
2022-09-18 17:17:50 +0300

    Added proxy

--- lib/dou_parser.rb
+++ lib/dou_parser.rb
@@ -1,12 +1,20 @@
 require 'httparty'
 require 'nokogiri'
-require 'byebug'
 
 class DouParser
   def initialize
     url = 'https://jobs.dou.ua/first-job/'
-    html = HTTParty.get(url)
-    @page = Nokogiri::HTML(html.body)
+    options = { http_proxyaddr: ENV['PROXY_ADDR'],
+                http_proxyport: ENV['PROXY_PORT'],
+                http_proxyuser: ENV['PROXY_USER'],
+                http_proxypass: ENV['PROXY_PASS']
+              }
+
+    responce = HTTParty.get(
+      url,
+      options
+    )
+    @page = Nokogiri::HTML(responce.body)
   end
 
   def result(language)
@@ -14,8 +22,7 @@ class DouParser
     events_include_lang = parsed_events.keys.select { |el| el.downcase.match(/\b#{language.downcase}\b/) }
     vacancies_include_lang = parsed_vacancies.keys.select { |el| el.downcase.match(/\b#{language.downcase}\b/)}
     if events_include_lang.empty? && vacancies_include_lang.empty?
-      res << 'Sorry, there are no events or vacancies for this language/technology for now :('
-      res
+      'Sorry, there are no events or vacancies for this language/technology for now :('
     else
       res << "Hooray! There are some events or vacancies for this language/technology.\n" \
           << "\n\n" \
@@ -46,7 +53,7 @@ class DouParser
     res = ""
     res << "<b>Events</b>: #{events.size}\n"
     parsed_events.each do |key, value|
-      res << "&#128165; #{key}: #{value}\n" if events.include?(key)
+      res << "\u{1F4A5} #{key}: #{value}\n" if events.include?(key)
     end
     res
   end
@@ -55,7 +62,7 @@ class DouParser
     res = ""
     res << "<b>Vacancies</b>: #{vacancies.size}\n"
     parsed_vacancies.each do |key, value|
-      res << "&#128165; #{key}: #{value}\n" if vacancies.include?(key)
+      res << "\u{1F4A5} #{key}: #{value}\n" if vacancies.include?(key)
     end
     res
   end
